9ff080fd6acdd6c64d7935b8ecdac2db62d899c0
t109318121 <74392671+t109318121@users.noreply.github.com>
2021-01-06 21:10:54 +0800

    update

--- KMeans.py
+++ KMeans.py
@@ -3,22 +3,33 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
-# import pylab
+import time
 
 df = pd.read_excel('120_data.xlsx', header=None)
 X = df.to_numpy()
+
+#開始計算kmeans時間
+start = time.time()
 
 # KMeans 演算法
 kmeans_fit = cluster.KMeans(n_clusters = 3).fit(X)
 
 # 印出分群結果
 cluster_labels = kmeans_fit.labels_
+kmeans_centers = kmeans_fit.cluster_centers_
 # print("分群結果：")
 # print(cluster_labels)
 # print("---")
+print('kmeans_centers = \n', kmeans_centers)
+
+# 結束測量
+end = time.time()
+print('執行時間：%f 秒 ' %(end-start))
 
 # plot
 fig = plt.figure()
 ax = Axes3D(fig)
-ax.scatter(X[:, 0], X[:, 1], X[:, 2], c=cluster_labels, cmap='Set1')
+ax.scatter(X[:, 0], X[:, 1], X[:, 2] , c=cluster_labels, cmap='Set1')
+ax.scatter(kmeans_centers[:, 0], kmeans_centers[:, 1], kmeans_centers[:, 2] , s=200 , c='rgb' , alpha=1,cmap='Set1', marker='*' )
+
 plt.show()
--- fcm.py
+++ fcm.py
@@ -3,12 +3,17 @@ from fcmeans import FCM
 from matplotlib import pyplot as plt
 import pandas as pd
 from mpl_toolkits.mplot3d import Axes3D
+import time
 
 n_samples = 3000
 
-df = pd.read_excel(r"C:\Users\Allen\Desktop\data120.xlsx", header=None)
+df = pd.read_excel("120_data.xlsx", header=None)
 X = df.to_numpy()
 
+#開始計算FCM時間
+start = time.time()
+
+#FCM分群
 fcm = FCM(n_clusters=3)
 fcm.fit(X)
 
@@ -16,8 +21,16 @@ fcm.fit(X)
 fcm_centers = fcm.centers
 fcm_labels = fcm.predict(X)
 
+#印群心
+print('fcm_centers = \n', fcm_centers)
+
+# 結束測量
+end = time.time()
+print("執行時間：%f 秒" %(end-start))
+
 # plot result
 fig = plt.figure()
 ax = Axes3D(fig)
 ax.scatter(X[:, 0], X[:, 1], X[:, 2], c=fcm_labels, cmap='Set1')
+ax.scatter(fcm_centers[:, 0], fcm_centers[:, 1], fcm_centers[:, 2],  s=200 , c='rgb' , alpha=1,cmap='Set1', marker='*')
 plt.show()
